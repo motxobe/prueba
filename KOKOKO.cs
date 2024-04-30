1bb76c22a5e1e3402a8d10505b52aa681d6a36cf
motxobe <72982830+motxobe@users.noreply.github.com>
2024-04-30 09:02:41 +0200

    lo añado a las 9

---
+++ KOKOKO.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Program
+{
+    class KOKOKO
+
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hoy es martes");
+            Console.Write(Environment.NewLine); // line break
+            Console.Write("What is your name? ");
+            string nombre = Console.ReadLine();
+            Console.WriteLine("Hello " + nombre);
+            DateTime fecha;
+            DateTime currentDate = DateTime.Now;
+            fecha = DateTime.Now;
+            int dia, mes, anio;
+            dia = fecha.Day;
+            mes = fecha.Month;
+            anio = fecha.Year;
+            Console.Write(Environment.NewLine + "Today is " + dia + "/" + mes + "/" + anio);
+            Console.Write(Environment.NewLine + "Press a key to exit... ");
+            Console.ReadLine(); // Console.ReadKey(true);
+        }
+    }
+}
+
