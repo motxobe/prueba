7b796bb8a84749270abb000b26ea0e4e4494dec8
motxobe <72982830+motxobe@users.noreply.github.com>
2024-08-05 17:05:15 +0200

    Add files via upload

---
+++ DTOLibroAgregar.cs
@@ -0,0 +1,19 @@
+﻿using MiBiblioteca.Validators;
+
+namespace MiBiblioteca.DTOs
+{
+    public class DTOLibroAgregar
+    {
+        public string Isbn { get; set; }
+        public string Titulo { get; set; }
+
+        [PaginasValidacion]
+        public int Paginas { get; set; }
+        public decimal Precio { get; set; }
+        [PesoArchivoValidacion(PesoMaximoEnMegaBytes: 4)]
+        [TipoArchivoValidacion(grupoTipoArchivo: GrupoTipoArchivo.Imagen)]
+        public IFormFile Foto { get; set; }
+        public int AutorId { get; set; }
+        public int EditorialId { get; set; }
+    }
+}
